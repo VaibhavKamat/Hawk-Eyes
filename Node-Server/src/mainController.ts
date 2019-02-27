bf7066232ce6b9a4602ba3c5a7b5ed28f82bc239
Ashish Naik <ashish_naik1@persistent.co.in>
2019-02-27 19:28:01 +0530

    Modifications:added new controller on node and added socket io to package

---
+++ Node-Server/src/mainController.ts
@@ -0,0 +1,24 @@
+import { Request, Response } from 'express'
+import * as DroneData from './dataService';
+import * as server from './server'
+import * as net from 'net'
+
+var http = require('http').Server(server);
+var io = require('socket.io')(http);
+var coordinates = {};
+
+const dronePort=0,droneHost=0;
+
+io.on('connection', function(socket){
+
+    io.emit('locationChanged', coordinates);
+	
+});
+
+
+
+
+//refrences
+/*
+https://www.hacksparrow.com/tcp-socket-programming-in-node-js.html
+*/
