17e7a801e95bcbbcf8c868b788bf7e88c1517013
sid <sidasaw93@gmail.com>
2019-03-06 23:48:45 +0530

    Added landing page

---
+++ UI-Server/src/app/app-routing.module.ts
@@ -0,0 +1,16 @@
+import { NgModule }             from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { DroneInfoComponent } from './drone-info/drone-info.component';
+ 
+const routes: Routes = [
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: 'home', component: HomeComponent },
+  { path: 'dashboard', component: DroneInfoComponent }
+];
+ 
+@NgModule({
+  imports: [ RouterModule.forRoot(routes) ],
+  exports: [ RouterModule ]
+})
+export class AppRoutingModule {}
