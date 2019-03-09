2e12d8073ece00680e28d2d77424cc8f76360055
baburao.parab <baburao.parab@powerschool.com>
2019-03-09 08:54:07 +0530

    added script file to run all services

---
+++ run-apps.bat
@@ -0,0 +1,4 @@
+start cmd /k "cd .\Python-Server && python app.py"
+start cmd /k "cd .\Python-Server && python video_feed.py"
+start cmd /k "cd .\Node-Server && npm start"
+start cmd /k "cd .\UI-Server && npm start"
