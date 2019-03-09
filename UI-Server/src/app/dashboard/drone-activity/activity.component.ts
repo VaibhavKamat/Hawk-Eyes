53e104e913780b354a1caee1aef038656f257d3a
Saurabh Mahambrey <saurabh_mahambrey@persistent.co.in>
2019-03-09 22:39:38 +0530

    changes to activity component

--- UI-Server/src/app/dashboard/drone-activity/activity.component.ts
+++ UI-Server/src/app/dashboard/drone-activity/activity.component.ts
@@ -1,24 +1,55 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, SimpleChanges, SimpleChange, setTestabilityGetter, OnChanges } from '@angular/core';
+import { Drone } from './../../classes/drone';
+import { droneObjMock } from 'src/app/classes/drone-mock';
 
 @Component({
   selector: 'app-activity',
   templateUrl: './activity.component.html',
   styleUrls: ['./activity.component.scss']
 })
-export class ActivityComponent implements OnInit {
-  locations: any[]= [
-    { status: "report_problem", location: 'Location A',time:" Few sec ago" },
-    { status: "check_circle_outline", location: 'Location B',time:" 1 min ago" },
-    { status: "check_circle_outline", location: 'Location C',time:" 4 min ago" },
-    { status: "check_circle_outline", location: 'Location D',time:" 5 min  ago" },
-    { status: "check_circle_outline", location: 'Location E',time:" 9 min ago" },
+export class ActivityComponent implements OnInit, OnChanges {
+private drone: Drone;
+  @Input()
+  set setDrone(val: Drone){
+  this.drone = val;
+  }
 
-  ];;
+  tempList: any[] = [];
+  activityList: any[] = [];
+  activitySize = 5;
+  activityCount = 0;
 
-  
+
+
   constructor() { }
 
   ngOnInit() {
   }
 
+  ngOnChanges(changes: SimpleChanges){
+    const changeData: SimpleChange = changes.item;
+    console.log("yeyeye");
+    this.updateActivityList(this.drone);
+    if(Object(this.drone.threatObj).keys.length !== -1)
+    {
+      console.log("threat");
+    }
+  }
+
+  updateActivityList(droneObj): void {
+    console.log('updated');
+    this.tempList.push({
+    locationInfo: droneObj.locationName,
+    timeData: new Date()
+  });
+
+    this.tempList = this.tempList.slice(this.activityList.length - 5);
+    this.tempList = this.tempList.reverse();
+    this.activityList = this.tempList;
 }
+
+
+ 
+
+  }
+
